c1f455ab50b7935933e821f7ee5a45174b2e2dcf
BlackPig95 <thisisblackpig@gmail.com>
2024-07-17 15:53:40 +0700

    khung backend project module 5

---
+++ src/main/java/com/ra/project_module5_reactjs/security/principal/UserDetailCustom.java
@@ -0,0 +1,37 @@
+package com.ra.project_module5_reactjs.security.principal;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class UserDetailCustom implements UserDetails
+{
+    private String authorizationType = "Bearer ";
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities()
+    {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword()
+    {
+        return "";
+    }
+
+    @Override
+    public String getUsername()
+    {
+        return "";
+    }
+}
