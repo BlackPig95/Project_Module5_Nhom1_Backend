8fb0ae9b09bdb09fcca4cd54cd4629941704f3d3
BlackPig95 <thisisblackpig@gmail.com>
2024-07-18 09:29:24 +0700

    khung database backend

---
+++ src/main/java/com/ra/project_module5_reactjs/model/entity/Payment.java
@@ -0,0 +1,29 @@
+package com.ra.project_module5_reactjs.model.entity;
+
+import com.ra.project_module5_reactjs.constant.PaymentStatusEnum;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "payment")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Payment
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    private Double amount;
+    private Date paymentDate;
+    private String paymentMethod;
+    private PaymentStatusEnum paymentStatus;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "booking_id", referencedColumnName = "id")
+    private Booking booking;
+}
---
+++ src/main/java/com/ra/project_module5_reactjs/model/entity/User.java
@@ -0,0 +1,45 @@
+package com.ra.project_module5_reactjs.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+    @Column(name = "birth_date")
+    private Date birthDate;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "loyalty_points")
+    private Integer loyaltyPoints;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "status")
+    private Boolean status;
+    @Column(name = "username")
+    private String username;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+}
