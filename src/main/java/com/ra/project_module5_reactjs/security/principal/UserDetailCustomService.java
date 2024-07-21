2dc2cae3f551edef23bd361d9fd16802b18efef4
BlackPig95 <thisisblackpig@gmail.com>
2024-07-21 16:38:24 +0700

    hieunt-feature-day1

--- src/main/java/com/ra/project_module5_reactjs/security/principal/UserDetailCustomService.java
+++ src/main/java/com/ra/project_module5_reactjs/security/principal/UserDetailCustomService.java
@@ -21,10 +21,11 @@ public class UserDetailCustomService implements UserDetailsService
 {
 
     @Autowired
-    private UserRepository userRepository ;
+    private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
         User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
@@ -41,21 +42,5 @@ public class UserDetailCustomService implements UserDetailsService
                 .username(user.getUsername())
                 .authorities(user.getRoles().stream().map(roles -> new SimpleGrantedAuthority(roles.getName().toString())).toList())
                 .build();
-
-    private final IUserRepo userRepo;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
-    {
-        Optional<User> optionalUser = userRepo.findByUsername(username);
-        if (optionalUser.isPresent())
-        {
-            User user = optionalUser.get();
-            return UserDetailCustom.builder()
-                    .username(user.getUsername())
-                    .build();
-        }
-        throw new UsernameNotFoundException("No such username exist");
-
     }
 }
