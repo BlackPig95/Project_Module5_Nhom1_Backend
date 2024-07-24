f634d360326f9547d41b5566ef8c12d6add1bd3b
BlackPig95 <thisisblackpig@gmail.com>
2024-07-24 09:46:15 +0700

    Task #9428 giao dien danh sach phim role user + Task #9430 phan trang danh sach phim role user

--- src/main/java/com/ra/project_module5_reactjs/controller/client/ClientMovieController.java
+++ src/main/java/com/ra/project_module5_reactjs/controller/client/ClientMovieController.java
@@ -5,14 +5,12 @@ import com.ra.project_module5_reactjs.model.entity.Movie;
 import com.ra.project_module5_reactjs.service.design.admin.IMovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -58,6 +56,24 @@ public class ClientMovieController
                 .statusCode(httpOk.value())
                 .status(httpOk)
                 .message("Danh sách các phim được tìm thấy")
+                .data(movies)
+                .build();
+    }
+
+    @GetMapping("/all/{pageNumber}")
+    public CustomResponseEntity<?> findMoviesPageable(@PageableDefault(page = 0, size = 10,
+            sort = "id", direction = Sort.Direction.ASC) Pageable pageable, @PathVariable("pageNumber") String pageNumber
+            , @RequestParam(value = "itemsPerPage") String itemsPerPage)
+    {
+        Pageable newPageable = pageable;
+        int numberOfPage = Integer.parseInt(pageNumber);
+        int numberOfItems = Integer.parseInt(itemsPerPage);
+        newPageable = PageRequest.of(numberOfPage, numberOfItems, pageable.getSort());
+        Page<Movie> movies = movieService.findAll(newPageable);
+        return CustomResponseEntity.builder()
+                .statusCode(httpOk.value())
+                .status(httpOk)
+                .message("Danh sách phim phân trang")
                 .data(movies)
                 .build();
     }
