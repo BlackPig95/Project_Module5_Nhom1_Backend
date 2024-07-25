7259aea2cb3f55dfaf9b85e497c76ba5a85fbc22
BlackPig95 <thisisblackpig@gmail.com>
2024-07-25 09:19:36 +0700

    Task #9433 xem danh sach tin tuc

---
+++ src/main/java/com/ra/project_module5_reactjs/controller/general/GeneralNewsController.java
@@ -0,0 +1,33 @@
+package com.ra.project_module5_reactjs.controller.general;
+
+import com.ra.project_module5_reactjs.model.dto.response.CustomResponseEntity;
+import com.ra.project_module5_reactjs.service.design.admin.INewsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/news")
+public class GeneralNewsController
+{
+    private final HttpStatus httpOk = HttpStatus.OK;
+    private final INewsService newsService;
+
+    @GetMapping({"/", ""})
+    public CustomResponseEntity<?> getAllNews(@PageableDefault(page = 0, size = 8,
+            sort = "id", direction = Sort.Direction.ASC) Pageable pageable)
+    {
+        return CustomResponseEntity.builder()
+                .statusCode(httpOk.value())
+                .status(httpOk)
+                .message("Danh sách tin tức")
+                .data(newsService.findAllNews(pageable))
+                .build();
+    }
+}
--- src/main/java/com/ra/project_module5_reactjs/repository/INewsRepository.java
+++ src/main/java/com/ra/project_module5_reactjs/repository/INewsRepository.java
@@ -2,6 +2,9 @@ package com.ra.project_module5_reactjs.repository;
 
 import com.ra.project_module5_reactjs.model.entity.News;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface INewsRepository extends JpaRepository<News,Long> {
+@Repository
+public interface INewsRepository extends JpaRepository<News, Long>
+{
 }
